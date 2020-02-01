f7e9ecd8d4819896cdd96a859cf5e73dea46e799
paulcrepin <mail@paulcrepin.com>
2020-02-01 05:27:40 +0100

    Update : Texture (need to fix) + player + move + skybox for fun in edit mode

---
+++ Licorne/Assets/Script/PlayerActions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script moves the character controller forward and backward
+/// </summary>
+public class PlayerActions : MonoBehaviour
+{
+    CharacterController characterController;
+
+    public float distance = 0.7f;
+    public GameObject MyCamera;
+    public float gravity = 20.0f;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3();
+        if (characterController.isGrounded)
+            moveDirection = MyCamera.transform.forward * distance;
+        moveDirection.y -= gravity * Time.deltaTime;
+        //Forward
+        if (Input.GetKey("up")) characterController.Move(moveDirection * Time.deltaTime);
+        //Backward
+        if (Input.GetKey("down")) characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
---
+++ Licorne/Assets/Script/PlayerView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script moves the Camera on the character on axis of the mouse
+/// </summary>
+public class PlayerView : MonoBehaviour
+{
+    public float speedH = 3.5f;
+    public float speedV = 3.5f;
+
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedH * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
