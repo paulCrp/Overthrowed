48e204a127987c6fe77f053d022e8239e0b3648d
paulcrepin <mail@paulcrepin.com>
2020-02-01 20:51:41 +0100

    Update : Stable version (merge with MatJeg version)

---
+++ Licorne/Assets/Script/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum State {
+        INIT,
+        FIRST_TRIGGER,
+        AFTER_FIRST,
+        SECOND_TRIGGER,
+        THIRD_TRIGGER
+    }
+
+    private State _currentState;
+    private GameObject _user;
+    private float _beginTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentState=State.INIT;
+        _user=GameObject.Find("Capsule");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTransition();
+    }
+
+    void CheckTransition(){
+        if(_currentState==State.INIT){
+            Vector3 userPos=_user.transform.position;
+            Vector3 firstTriggerPos=GameObject.Find("firstTrigger").transform.position;
+            Debug.Log(Vector3.Distance(userPos,firstTriggerPos));
+            if(Vector3.Distance(userPos,firstTriggerPos)<4){
+                _currentState=State.FIRST_TRIGGER;
+                _beginTime=Time.time;
+                TriggerSound();
+            }
+        } else if(_currentState==State.FIRST_TRIGGER){
+            float currentTime=Time.time;
+            if(currentTime-_beginTime>3){
+                _currentState=State.AFTER_FIRST;
+            }
+        }
+    }
+
+    void TriggerSound(){
+        PlayAnim audioAnim=GameObject.Find("audioSource").GetComponent<PlayAnim>();
+        audioAnim.enabled=true;
+
+    }
+
+    void StopSound(){
+        GameObject audioSource=GameObject.Find("audioSource");
+        AudioSource audio=audioSource.GetComponent<AudioSource>();
+        audioSource.transform.parent=null;
+    }
+}
---
+++ Licorne/Assets/Script/PlayAnim.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnim : MonoBehaviour
+{
+    enum State{
+        INIT,
+        FIRST,
+        SECOND,
+        FINAL
+    }
+
+    public AudioClip _clip;
+    private float _beginTime;
+    private State _currentState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _beginTime=Time.time;
+        _currentState=State.INIT;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_currentState==State.INIT){
+            GameObject user=GameObject.Find("Capsule");
+            Vector3 userPos=user.transform.position;
+            GameObject audioSource=GameObject.Find("audioSource");
+            AudioSource audio = gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+            audio.clip=_clip;
+            audio.spatialBlend=1;
+            audio.enabled=true;
+            audio.minDistance=1;
+            audio.maxDistance=3;
+            Vector3 shift=new Vector3(1,0,5);
+            audioSource.transform.parent=user.transform;
+            audioSource.transform.position=userPos-shift;
+            audio.Play();
+            _currentState=State.FIRST;
+        } else if(_currentState==State.FIRST){
+            float currentTime=Time.time;
+            if(currentTime-_beginTime>1){
+                GameObject audioSource=GameObject.Find("audioSource");
+                AudioSource audio=audioSource.GetComponent<AudioSource>();
+                Destroy(audio);
+                _currentState=State.SECOND;
+            }
+        } else if(_currentState==State.SECOND){
+            GameObject user=GameObject.Find("Capsule");
+            Vector3 userPos=user.transform.position;
+            GameObject audioSource=GameObject.Find("audioSource");
+            AudioSource audio = gameObject.AddComponent(typeof(AudioSource)) as AudioSource;
+            audio.clip=_clip;
+            audio.spatialBlend=1;
+            audio.enabled=true;
+            audio.minDistance=1;
+            audio.maxDistance=3;
+            Vector3 shift=new Vector3(1,0,-5);
+            audioSource.transform.parent=user.transform;
+            audioSource.transform.position=userPos-shift;
+            // audio.Play();
+            _currentState=State.FINAL;
+        }
+    }
+}
